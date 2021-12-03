e2deb15505592e0c7915e8f495e3613e1875a55b
Om More <51229945+thisisommore@users.noreply.github.com>
2021-12-03 16:38:27 +0530

    Main: Listen on start

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "netsepio-api/app"
+import (
+	"log"
+	"netsepio-api/app"
+)
 
 func main() {
 	app.Init()
+	err := app.GinApp.Run(":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
