3240cdd5f05e3915e0069c6899225b44eff0d956
Om More <51229945+thisisommore@users.noreply.github.com>
2021-12-06 18:19:20 +0530

    JSON: Provide struct tags for user model Fixes #6

--- models/User.go
+++ models/User.go
@@ -3,10 +3,10 @@ package models
 import "github.com/lib/pq"
 
 type User struct {
-	Name              string
-	WalletAddress     string   `gorm:"primary_key"`
-	FlowIds           []FlowId `gorm:"foreignkey:WalletAddress"`
-	ProfilePictureUrl string
-	Country           string
-	Roles             pq.Int32Array `gorm:"type:int[]"`
+	Name              string        `json:"name,omitempty"`
+	WalletAddress     string        `gorm:"primary_key" json:"walletAddress"`
+	FlowIds           []FlowId      `gorm:"foreignkey:WalletAddress" json:"-"`
+	ProfilePictureUrl string        `json:"profilePictureUrl,omitempty"`
+	Country           string        `json:"country,omitempty"`
+	Roles             pq.Int32Array `gorm:"type:int[]" json:"roles"`
 }
