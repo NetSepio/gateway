020d1c0c409830491f0526847b85abb420cbf752
thisisommore <ommore501@gmail.com>
2022-01-15 19:13:49 +0530

    logwrapper: add more log functions

--- util/pkg/logwrapper/exports.go
+++ util/pkg/logwrapper/exports.go
@@ -1,9 +1,21 @@
 package logwrapper
 
-func Fatal(args ...interface{})  {
+func Fatal(args ...interface{}) {
 	Log.Fatal(args...)
 }
 
-func Fatalf(format string,args ...interface{})  {
-	Log.Fatalf(format,args...)
+func Fatalf(format string, args ...interface{}) {
+	Log.Fatalf(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	Log.Warnf(format, args...)
+}
+
+func Warn(args ...interface{}) {
+	Log.Warn(args...)
+}
+
+func Info(args ...interface{}) {
+	Log.Info(args...)
 }
