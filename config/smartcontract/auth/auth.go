e0b1154245dcacacf51f1f7365f782d38b91c2bd
thisisommore <ommore501@gmail.com>
2022-01-15 21:47:10 +0530

    config(auth): don't export auth instead return it using GetAuth

--- config/smartcontract/auth/auth.go
+++ config/smartcontract/auth/auth.go
@@ -15,13 +15,13 @@ import (
 )
 
 /* Global Auth variable can be accessed for auth purposes in smart contract transactions */
-var Auth *bind.TransactOpts
+var auth *bind.TransactOpts
 
-func InitAuth(client *ethclient.Client) *bind.TransactOpts {
+func GetAuth(client *ethclient.Client) *bind.TransactOpts {
 
-	if Auth != nil {
+	if auth != nil {
 		//Auth is already initialized, return that object
-		return Auth
+		return auth
 	}
 
 	mnemonic := os.Getenv("MNEMONIC")
@@ -35,7 +35,7 @@ func InitAuth(client *ethclient.Client) *bind.TransactOpts {
 		log.Fatal(err)
 	}
 
-	gasLimit := uint64(21000) // in units
+	gasLimit := uint64(200000) // in units
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		log.Fatal(err)
@@ -45,13 +45,13 @@ func InitAuth(client *ethclient.Client) *bind.TransactOpts {
 	if err != nil {
 		logwrapper.Fatalf("failed to get network id, error: %v", err.Error())
 	}
-	Auth, err = bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	auth, err = bind.NewKeyedTransactorWithChainID(privateKey, chainID)
 	if err != nil {
 		logwrapper.Fatalf("failed to call NewKeyedTransactorWithChainID, error: %v", err.Error())
 	}
-	Auth.Nonce = big.NewInt(int64(nonce))
-	Auth.Value = big.NewInt(0) // in wei
-	Auth.GasLimit = gasLimit   // in units
-	Auth.GasPrice = gasPrice
-	return Auth
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0) // in wei
+	auth.GasLimit = gasLimit   // in units
+	auth.GasPrice = gasPrice
+	return auth
 }
