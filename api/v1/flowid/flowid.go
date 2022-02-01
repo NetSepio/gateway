d45eb8103512ab5205ad4c3d3064b5d800faa7fd
thisisommore <ommore501@gmail.com>
2022-02-01 10:34:02 +0000

    Send EULA even if user is registered

--- api/v1/flowid/flowid.go
+++ api/v1/flowid/flowid.go
@@ -3,14 +3,11 @@ package flowid
 import (
 	"net/http"
 
-	"github.com/TheLazarusNetwork/marketplace-engine/config/dbconfig"
 	"github.com/TheLazarusNetwork/marketplace-engine/models"
 	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/flowid"
 	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/httphelper"
-	"github.com/TheLazarusNetwork/marketplace-engine/util/pkg/logwrapper"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -23,52 +20,23 @@ func ApplyRoutes(r *gin.RouterGroup) {
 }
 
 func getFlowId(c *gin.Context) {
-	db := dbconfig.GetDb()
-	var user models.User
 	walletAddress := c.Query("walletAddress")
 	if walletAddress == "" {
 		httphelper.ErrResponse(c, http.StatusBadRequest, "Wallet address (walletAddress) is required")
 		return
 	}
-	dbRes := db.Model(&models.User{}).Where("wallet_address = ?", walletAddress).First(&user)
-	// If there is an error and that error is not of "record not found"
-	if dbRes.Error != nil && dbRes.Error != gorm.ErrRecordNotFound {
-		log.Error(dbRes.Error)
+
+	flowId, err := flowid.GenerateFlowId(walletAddress, false, models.AUTH, "")
+	if err != nil {
+		log.Error(err)
 		httphelper.ErrResponse(c, http.StatusInternalServerError, "Unexpected error occured")
+
 		return
 	}
-	// If wallet address exist
-	if dbRes.Error != gorm.ErrRecordNotFound {
-		flowId, err := flowid.GenerateFlowId(walletAddress, true, models.AUTH, "")
-		if err != nil {
-			log.Error(err)
-			httphelper.ErrResponse(c, http.StatusInternalServerError, "Unexpected error occured")
-
-			return
-		}
-		payload := GetFlowIdPayload{
-			FlowId: flowId,
-		}
-		httphelper.SuccessResponse(c, "Flowid successfully generated", payload)
-	} else {
-		//If wallet address doesn't exist
-		flowId, err := flowid.GenerateFlowId(walletAddress, false, models.AUTH, "")
-		if err != nil {
-			log.Error(err)
-			httphelper.ErrResponse(c, http.StatusInternalServerError, "Unexpected error occured")
-
-			return
-		}
-		if err != nil {
-			logwrapper.Error(err)
-			httphelper.ErrResponse(c, 500, "Unexpected error occured")
-			return
-		}
-		userAuthEULA := "TODO AUTH EULA"
-		payload := GetFlowIdPayload{
-			FlowId: flowId,
-			Eula:   userAuthEULA,
-		}
-		httphelper.SuccessResponse(c, "Flowid successfully generated", payload)
+	userAuthEULA := "TODO AUTH EULA"
+	payload := GetFlowIdPayload{
+		FlowId: flowId,
+		Eula:   userAuthEULA,
 	}
+	httphelper.SuccessResponse(c, "Flowid successfully generated", payload)
 }
