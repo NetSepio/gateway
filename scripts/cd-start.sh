9c63ec6923b71fb85f22b23c6ada84e20d08eecd
thisisommore <ommore501@gmail.com>
2022-02-03 11:45:12 +0000

    Update scripts

--- scripts/cd-start.sh
+++ scripts/cd-start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd /home/ubuntu/Gateway
-docker stop gateway && docker rm gateway
+docker stop gateway
 docker image rm 560914023379.dkr.ecr.us-east-2.amazonaws.com/netsepio-gateway --force
-docker run -d --name="gateway" \
---add-host="postgres:host-gateway" \
+docker run -d --rm \
+    --name="gateway" \
+    -p 3000:3000 \
+    --add-host="postgres:host-gateway" \
     -v $(pwd):/app/logs \
     --env-file=.env \
     560914023379.dkr.ecr.us-east-2.amazonaws.com/netsepio-gateway
