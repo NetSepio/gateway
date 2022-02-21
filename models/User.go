47e6e2ec5515badd340751f7004ea762c7436f43
thisisommore <ommore501@gmail.com>
2022-02-21 10:09:29 +0000

    Remove userroles entity from database

--- models/User.go
+++ models/User.go
@@ -1,10 +1,9 @@
 package models
 
 type User struct {
-	Name              string     `json:"name,omitempty"`
-	WalletAddress     string     `gorm:"primary_key" json:"walletAddress"`
-	FlowIds           []FlowId   `gorm:"foreignkey:WalletAddress" json:"-"`
-	ProfilePictureUrl string     `json:"profilePictureUrl,omitempty"`
-	Country           string     `json:"country,omitempty"`
-	Roles             []UserRole `gorm:"foreignkey:WalletAddress;type:int[];not null" json:"roles"`
+	Name              string   `json:"name,omitempty"`
+	WalletAddress     string   `gorm:"primary_key" json:"walletAddress"`
+	FlowIds           []FlowId `gorm:"foreignkey:WalletAddress" json:"-"`
+	ProfilePictureUrl string   `json:"profilePictureUrl,omitempty"`
+	Country           string   `json:"country,omitempty"`
 }
--- models/UserRole.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type UserRole struct {
-	WalletAddress string `gorm:"unique"`
-	RoleId        string `gorm:"unique"`
-}
