e4897c74e884a7047a4d1fdbb041e195190ff419
thisisommore <ommore501@gmail.com>
2022-02-26 08:15:04 +0000

    Add cors middleware

--- app/app.go
+++ app/app.go
@@ -1,13 +1,16 @@
 package app
 
 import (
+	"time"
+
 	"github.com/TheLazarusNetwork/netsepio-engine/api"
 	"github.com/TheLazarusNetwork/netsepio-engine/config"
+	"github.com/TheLazarusNetwork/netsepio-engine/util/pkg/envutil"
 	"github.com/TheLazarusNetwork/netsepio-engine/util/pkg/logwrapper"
 
 	"github.com/TheLazarusNetwork/netsepio-engine/config/dbconfig"
 	"github.com/TheLazarusNetwork/netsepio-engine/config/netsepio"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,7 +20,15 @@ func Init(envPath string, logBasePath string) {
 	config.Init(envPath)
 	logwrapper.Init(logBasePath)
 	netsepio.InitRolesId()
+
 	GinApp = gin.Default()
+
+	corsM := cors.New(cors.Config{AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+		AllowOrigins:     []string{envutil.MustGetEnv("ALLOWED_ORIGIN")}})
+	GinApp.Use(corsM)
 	api.ApplyRoutes(GinApp)
 	dbconfig.GetDb()
 }
