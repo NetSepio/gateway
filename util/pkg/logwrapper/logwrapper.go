5ea3263f98acf0bf7135ebef1f250fe10b94bde4
thisisommore <ommore501@gmail.com>
2022-02-26 14:47:03 +0530

    Log if LOG_TO_FILE is set to true

--- util/pkg/logwrapper/logwrapper.go
+++ util/pkg/logwrapper/logwrapper.go
@@ -31,14 +31,16 @@ func Init(basepath string) {
 	}
 	Log.Logger.SetFormatter(&logrus.JSONFormatter{})
 
-	timeNow := strings.Replace(time.Now().Format(time.UnixDate), ":", "_", -1)
-	fileName := fmt.Sprintf("%v.log", timeNow)
-	filePath := filepath.Join(basepath, fileName)
-	file, err := os.Create(filePath)
-	if err != nil {
-		Log.Fatalf("Error creating log file: %v", err)
+	if os.Getenv("LOG_TO_FILE") == "true" {
+		timeNow := strings.Replace(time.Now().Format(time.UnixDate), ":", "_", -1)
+		fileName := fmt.Sprintf("%v.log", timeNow)
+		filePath := filepath.Join(basepath, fileName)
+		file, err := os.Create(filePath)
+		if err != nil {
+			Log.Fatalf("Error creating log file: %v", err)
+		}
+		writer := io.MultiWriter(file, os.Stdout)
+		Log.Logger.SetOutput(writer)
+		gin.DefaultWriter = writer
 	}
-	writer := io.MultiWriter(file, os.Stdout)
-	Log.Logger.SetOutput(writer)
-	gin.DefaultWriter = writer
 }
