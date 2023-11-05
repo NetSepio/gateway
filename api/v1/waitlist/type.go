68c46e70a7ce5881f47c310f885f7f4128a6c943
thisisommore <ommore501@gmail.com>
2023-11-05 06:45:21 +0000

    Twitter username should start with `@`

--- api/v1/waitlist/type.go
+++ api/v1/waitlist/type.go
@@ -3,5 +3,5 @@ package waitlist
 type WaitListRequest struct {
 	EmailId       string `binding:"required,email" json:"emailId"`
 	WalletAddress string `json:"walletAddress,omitempty" binding:"omitempty,hexadecimal,startswith=0x"`
-	Twitter       string `json:"twitter,omitempty"`
+	Twitter       string `json:"twitter,omitempty" binding:"omitempty,startswith=@"`
 }
