8d4a2dcddc62cf6d4ffeff37cb433e37490be305
Om More <51229945+thisisommore@users.noreply.github.com>
2023-11-10 19:48:56 +0530

    Introduce delete review api (#65)
    * Introduce delete review api
    
    * get wallet address from middleware using exported key

--- api/v1/v1.go
+++ api/v1/v1.go
@@ -3,6 +3,7 @@ package apiv1
 import (
 	authenticate "github.com/NetSepio/gateway/api/v1/authenticate"
 	delegatereviewcreation "github.com/NetSepio/gateway/api/v1/delegateReviewCreation"
+	"github.com/NetSepio/gateway/api/v1/deletereview"
 	"github.com/NetSepio/gateway/api/v1/feedback"
 	flowid "github.com/NetSepio/gateway/api/v1/flowid"
 	"github.com/NetSepio/gateway/api/v1/getreviews"
@@ -22,6 +23,7 @@ func ApplyRoutes(r *gin.RouterGroup) {
 		authenticate.ApplyRoutes(v1)
 		profile.ApplyRoutes(v1)
 		delegatereviewcreation.ApplyRoutes(v1)
+		deletereview.ApplyRoutes(v1)
 		healthcheck.ApplyRoutes(v1)
 		feedback.ApplyRoutes(v1)
 		waitlist.ApplyRoutes(v1)
--- models/Review.go
+++ models/Review.go
@@ -1,18 +1,23 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Review struct {
-	Voter              string    `json:"voter" binding:"required,hexadecimal"`
-	MetaDataUri        string    `json:"metaDataUri" binding:"required"`
-	Category           string    `json:"category" binding:"required"`
-	DomainAddress      string    `json:"domainAddress" binding:"required"`
-	SiteUrl            string    `json:"siteUrl" binding:"required"`
-	SiteType           string    `json:"siteType" binding:"required"`
-	SiteTag            string    `json:"siteTag" binding:"required"`
-	SiteSafety         string    `json:"siteSafety" binding:"required"`
-	SiteIpfsHash       string    `json:"siteIpfsHash" binding:"required"`
-	TransactionHash    string    `json:"transactionHash" gorm:"primary_key"`
-	TransactionVersion int64     `json:"transactionVersion"`
+	Voter              string `json:"voter" binding:"required,hexadecimal"`
+	MetaDataUri        string `json:"metaDataUri" binding:"required" gorm:"primary_key"`
+	Category           string `json:"category" binding:"required"`
+	DomainAddress      string `json:"domainAddress" binding:"required"`
+	SiteUrl            string `json:"siteUrl" binding:"required"`
+	SiteType           string `json:"siteType" binding:"required"`
+	SiteTag            string `json:"siteTag" binding:"required"`
+	SiteSafety         string `json:"siteSafety" binding:"required"`
+	SiteIpfsHash       string `json:"siteIpfsHash" binding:"required"`
+	TransactionHash    string `json:"transactionHash"`
+	TransactionVersion int64  `json:"transactionVersion"`
+	DeletedAt          gorm.DeletedAt
 	CreatedAt          time.Time `json:"createdAt"`
 }
