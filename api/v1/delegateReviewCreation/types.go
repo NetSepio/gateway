e0f2170adb54cb5d167faa02d08c23eac8e9ebd1
thisisommore <ommore501@gmail.com>
2023-11-11 16:24:00 +0000

    remove http_url validation from DomainAddress

--- api/v1/delegateReviewCreation/types.go
+++ api/v1/delegateReviewCreation/types.go
@@ -3,7 +3,7 @@ package delegatereviewcreation
 type DelegateReviewCreationRequest struct {
 	MetaDataUri   string `json:"metaDataUri" binding:"required"`
 	Category      string `json:"category" binding:"required"`
-	DomainAddress string `json:"domainAddress" binding:"required,http_url"`
+	DomainAddress string `json:"domainAddress" binding:"required"`
 	SiteUrl       string `json:"siteUrl" binding:"required,http_url"`
 	SiteType      string `json:"siteType" binding:"required"`
 	SiteTag       string `json:"siteTag" binding:"required"`
