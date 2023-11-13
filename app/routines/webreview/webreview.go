56987db909a41ac13f27c9041667a53e85dec940
thisisommore <ommore501@gmail.com>
2023-11-13 05:42:33 +0000

    archive webreview: return on error

--- app/routines/webreview/webreview.go
+++ app/routines/webreview/webreview.go
@@ -31,12 +31,12 @@ func UploadToIpfs(osFile io.Reader, fileName string) (*NFTStorageRes, error) {
 	// Create a new form field
 	fw, err := w.CreateFormFile("file", fileName)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create file %s: %w", fileName, err)
 	}
 
 	// Copy the contents of the file to the form field
 	if _, err := io.Copy(fw, osFile); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to copy contents to %s: %w", fileName, err)
 	}
 
 	// Close the multipart writer to finalize the request
@@ -45,22 +45,22 @@ func UploadToIpfs(osFile io.Reader, fileName string) (*NFTStorageRes, error) {
 	// Send the request
 	req, err := http.NewRequest("POST", "https://api.nft.storage/upload", &buf)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create new request for nft.storage: %w", err)
 	}
 	req.Header.Set("Content-Type", w.FormDataContentType())
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", envconfig.EnvVars.NFT_STORAGE_KEY))
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to do request for nft.storage: %w", err)
 	}
 	bodyBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read body of request for nft.storage: %w", err)
 	}
 	nftRes, err := UnmarshalNFTStorageRes(bodyBytes)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to unmarshal json body of request for nft.storage: %w", err)
 	}
 	defer resp.Body.Close()
 	return &nftRes, nil
@@ -73,28 +73,32 @@ func Publish(siteUrl string) {
 	dirName := path.Join("storage", uid_str)
 	err := os.Mkdir(dirName, os.ModePerm)
 	if err != nil {
-		logwrapper.Warnf("failed to create folder %v, error: %v", dirName, err.Error())
+		logwrapper.Errorf("failed to create folder %v, error: %v", dirName, err.Error())
 	}
 
 	websiteURL := siteUrl
 	domain, err := url.Parse(websiteURL)
 	if err != nil {
-		logwrapper.Warnf("failed to parse url, error: %v", err.Error())
+		logwrapper.Errorf("failed to parse url, error: %v", err.Error())
+		return
 	}
 	htmlFile := domain.Host
 
 	err = ws.CheckDomain(dirName, websiteURL)
 	if err != nil {
-		logwrapper.Warnf("failed to checkDomain for websiteURL : %v, error: %v", websiteURL, err.Error())
+		logwrapper.Errorf("failed to checkDomain for websiteURL : %v, error: %v", websiteURL, err.Error())
+		return
 	}
 	filePath := path.Join(dirName, htmlFile)
 	indexFile, err := os.Open(filePath)
 	if err != nil {
-		logwrapper.Warnf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		logwrapper.Errorf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		return
 	}
 	indexFileUploadRes, err := UploadToIpfs(indexFile, "index.html")
 	if err != nil {
-		logwrapper.Warnf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		logwrapper.Errorf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		return
 	}
 
 	// create context
@@ -104,20 +108,24 @@ func Publish(siteUrl string) {
 	var buf []byte
 	// capture entire browser viewport, returning png with quality=90
 	if err := chromedp.Run(ctx, ws.FullScreenshot(websiteURL, 90, &buf)); err != nil {
-		logwrapper.Warnf("failed to run chromedp, error: %v", err.Error())
+		logwrapper.Errorf("failed to run chromedp, error: %v", err.Error())
+		return
 	}
 	fileName := fmt.Sprintf("%v/fullScreenshot.png", dirName)
 	if err := os.WriteFile(fileName, buf, 0644); err != nil {
-		logwrapper.Warnf("failed to write file: %v, error:%v", fileName, err)
+		logwrapper.Errorf("failed to write file: %v, error:%v", fileName, err)
+		return
 	}
 
 	screenShotFile, err := os.Open(fileName)
 	if err != nil {
-		logwrapper.Warnf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		logwrapper.Errorf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		return
 	}
 	screenShotFileUploadRes, err := UploadToIpfs(screenShotFile, "screenshot.png")
 	if err != nil {
-		logwrapper.Warnf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		logwrapper.Errorf("failed to add file: %v to ipfs for websiteURL : %v, error: %v", filePath, websiteURL, err.Error())
+		return
 	}
 
 	metaData := MetaData{
@@ -126,12 +134,14 @@ func Publish(siteUrl string) {
 	}
 	data, err := json.Marshal(&metaData)
 	if err != nil {
-		logwrapper.Warnf("failed to marshal JSON for uid_str %v : %s", uid_str, err)
+		logwrapper.Errorf("failed to marshal JSON for uid_str %v : %s", uid_str, err)
+		return
 	}
 
 	metaDataRes, err := UploadToIpfs(bytes.NewReader(data), "metadata.json")
 	if err != nil {
-		logwrapper.Warnf("failed to add file to ipfs for fullScreenShot : %v", err.Error())
+		logwrapper.Errorf("failed to add file to ipfs for fullScreenShot : %v", err.Error())
+		return
 	}
 
 	//TODO send update tx
@@ -146,7 +156,7 @@ func Publish(siteUrl string) {
 	}
 	err = os.RemoveAll(dirName)
 	if err != nil {
-		logwrapper.Warnf("failed to remove dir %v, error:%v", dirName, err.Error())
+		logwrapper.Errorf("failed to remove dir %v, error:%v", dirName, err.Error())
 		return
 	}
 	cancel()
