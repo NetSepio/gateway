b3bd20817da3b652688c426c1d976901d4414383
thisisommore <ommore501@gmail.com>
2023-11-14 05:55:58 +0000

    Fix update profile and get profile

--- api/v1/profile/profile.go
+++ api/v1/profile/profile.go
@@ -1,20 +1,12 @@
 package profile
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/NetSepio/gateway/api/middleware/auth/paseto"
 	"github.com/NetSepio/gateway/config/dbconfig"
-	"github.com/NetSepio/gateway/config/envconfig"
 	"github.com/NetSepio/gateway/models"
-	"github.com/NetSepio/gateway/util/pkg/logwrapper"
 	"github.com/TheLazarusNetwork/go-helpers/httpo"
 
 	"github.com/gin-gonic/gin"
@@ -39,10 +31,16 @@ func patchProfile(c *gin.Context) {
 		httpo.NewErrorResponse(http.StatusForbidden, "payload is invalid").SendD(c)
 		return
 	}
+
+	profileUpdate := models.User{
+		Name:              requestBody.Name,
+		ProfilePictureUrl: requestBody.ProfilePictureUrl,
+		Country:           requestBody.Country,
+	}
 	walletAddress := c.GetString(paseto.CTX_WALLET_ADDRES)
 	result := db.Model(&models.User{}).
-		Where("wallet_address = ?", walletAddress).
-		Updates(requestBody)
+		Where("wallet_address = ?", strings.ToLower(walletAddress)).
+		Updates(&profileUpdate)
 	if result.Error != nil {
 		httpo.NewErrorResponse(http.StatusInternalServerError, "Unexpected error occured").SendD(c)
 
@@ -61,80 +59,15 @@ func getProfile(c *gin.Context) {
 	db := dbconfig.GetDb()
 	walletAddress := c.GetString(paseto.CTX_WALLET_ADDRES)
 	var user models.User
-	err := db.Model(&models.User{}).Select("name, profile_picture_url,country, wallet_address").Where("wallet_address = ?", walletAddress).First(&user).Error
+	err := db.Model(&models.User{}).Select("name, profile_picture_url,country, wallet_address").Where("wallet_address = ?", strings.ToLower(walletAddress)).First(&user).Error
 	if err != nil {
 		logrus.Error(err)
 		httpo.NewErrorResponse(http.StatusInternalServerError, "Unexpected error occured").SendD(c)
 		return
 	}
 
-	roles, err := getRoles(user.WalletAddress)
-	if err != nil {
-		logwrapper.Errorf("Failed to fetch roles from graph api %s", err)
-		return
-	}
 	payload := GetProfilePayload{
-		user.Name, user.WalletAddress, user.ProfilePictureUrl, user.Country, roles,
+		user.Name, user.WalletAddress, user.ProfilePictureUrl, user.Country,
 	}
 	httpo.NewSuccessResponseP(200, "Profile fetched successfully", payload).SendD(c)
-}
-
-type rolesResponse struct {
-	Data struct {
-		User struct {
-			Roles []string `json:"roles"`
-		} `json:"user"`
-	} `json:"data"`
-
-	Errors []struct {
-		Message string `json:"message"`
-	} `json:"errors"`
-}
-
-var (
-	errStatusCode = errors.New("status code is not 200")
-)
-
-func getRoles(walletAddress string) ([]string, error) {
-	jsonData := map[string]string{
-		"query": fmt.Sprintf(`
-		{ 
-			user(id:"%v"){
-				roles
-			}
-		}
-	`, strings.ToLower(walletAddress)),
-	}
-
-	jsonValue, _ := json.Marshal(jsonData)
-
-	request, err := http.NewRequest("POST", envconfig.EnvVars.GRAPH_API, bytes.NewBuffer(jsonValue))
-	if err != nil {
-		return []string{}, err
-	}
-	client := &http.Client{Timeout: time.Second * 10}
-	response, err := client.Do(request)
-
-	if err != nil {
-		return []string{}, err
-	}
-
-	if response.StatusCode != 200 {
-		return []string{}, errStatusCode
-	}
-	data, _ := io.ReadAll(response.Body)
-	var res rolesResponse
-	err = json.Unmarshal(data, &res)
-	if err != nil {
-		return []string{}, err
-	}
-
-	if len(res.Errors) > 1 {
-		return []string{}, errors.New(res.Errors[0].Message)
-	}
-	err = response.Body.Close()
-	if err != nil {
-		logwrapper.Warnf("failed to close body, error :%s", err)
-	}
-	return res.Data.User.Roles, nil
 }
