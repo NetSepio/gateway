bd2813c435dc31e903319338e76d722dce3af72e
thisisommore <ommore501@gmail.com>
2023-11-24 15:18:22 +0000

    delegate review: validate domain address

--- api/v1/delegateReviewCreation/types.go
+++ api/v1/delegateReviewCreation/types.go
@@ -3,7 +3,7 @@ package delegatereviewcreation
 type DelegateReviewCreationRequest struct {
 	MetaDataUri   string `json:"metaDataUri" binding:"required"`
 	Category      string `json:"category" binding:"required"`
-	DomainAddress string `json:"domainAddress" binding:"required"`
+	DomainAddress string `json:"domainAddress" binding:"required,hostname_rfc1123|hostname_port"`
 	SiteUrl       string `json:"siteUrl" binding:"required,http_url"`
 	SiteType      string `json:"siteType" binding:"required"`
 	SiteTag       string `json:"siteTag" binding:"required"`
