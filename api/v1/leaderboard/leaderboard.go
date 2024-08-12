f27197f751afb19e65d54e27f16bd174281bd1bd
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-08-12 13:09:25 +0530

    leaderboard api added

--- api/v1/leaderboard/leaderboard.go
+++ api/v1/leaderboard/leaderboard.go
@@ -1,5 +1,3 @@
-package leaderboard
-
 // ApplyRoutes applies router to gin Router
 // func ApplyRoutes(r *gin.RouterGroup) {
 // 	g := r.Group("/reviewerdetails")
@@ -22,3 +20,43 @@ package leaderboard
 // 	}
 // 	httpo.NewSuccessResponseP(200, "Profile fetched successfully", payload).SendD(c)
 // }
+
+package leaderboard
+
+import (
+	"net/http"
+
+	"github.com/NetSepio/gateway/config/dbconfig"
+	"github.com/NetSepio/gateway/models"
+	"github.com/NetSepio/gateway/util/pkg/logwrapper"
+	"github.com/TheLazarusNetwork/go-helpers/httpo"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ApplyRoutes applies router to gin Router
+func ApplyRoutes(r *gin.RouterGroup) {
+	g := r.Group("/getleaderboard")
+	{
+		g.GET("", getLeaderboard)
+	}
+}
+
+func getLeaderboard(c *gin.Context) {
+	db := dbconfig.GetDb()
+
+	var leaderboard []models.Leaderboard
+
+	if err := db.Order("reviews desc").Find(&leaderboard).Error; err != nil {
+		httpo.NewErrorResponse(http.StatusInternalServerError, "Unexpected error occurred").SendD(c)
+		logwrapper.Error("failed to get leaderboard", err)
+		return
+	}
+
+	if len(leaderboard) == 0 {
+		httpo.NewErrorResponse(404, "No leaderboard entries found").SendD(c)
+		return
+	}
+
+	httpo.NewSuccessResponseP(200, "Leaderboard fetched successfully", leaderboard).SendD(c)
+}
