071d895f8bd07406561816792ecf154ec9e87b68
Shubham Prajapati <shubhamprajapati032@gmail.com>
2025-02-15 12:34:15 +0530

    `Update deploy workflow to use ubuntu-latest and remove dbconfig migration in app initialization`

--- app/app.go
+++ app/app.go
@@ -10,7 +10,6 @@ import (
 	"github.com/stripe/stripe-go/v76"
 
 	"github.com/NetSepio/gateway/config/constants"
-	"github.com/NetSepio/gateway/config/dbconfig"
 	"github.com/NetSepio/gateway/config/envconfig"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -20,7 +19,7 @@ var GinApp *gin.Engine
 
 func Init() {
 	envconfig.InitEnvVars()
-	dbconfig.Migrate()
+	// dbconfig.Migrate()
 	stripe.Key = envconfig.EnvVars.STRIPE_SECRET_KEY
 	constants.InitConstants()
 	logwrapper.Init()
